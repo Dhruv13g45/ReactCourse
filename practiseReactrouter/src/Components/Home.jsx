cd930b48c8dd40a2fcb77c9abf01a34f5fd8d2a9
Dhruv Goradia <dhruvgoradia13@gmail.com>
2024-04-24 11:11:02 +0530

    practising react routing

---
+++ practiseReactrouter/src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <>
+            <div className='container flex gap-5 h-80 justify-evenly m-10'>
+                <div className="img h-full w-full">
+                    <img src="https://images.pexels.com/photos/6031345/pexels-photo-6031345.jpeg" alt="randomimage" className='h-full w-full' />
+                </div>
+                <div className="img_info">
+                    <h1 className='text-xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, voluptatem!</h1>
+                    <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quidem, sit molestias delectus optio totam quae corrupti consequuntur soluta atque?</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
